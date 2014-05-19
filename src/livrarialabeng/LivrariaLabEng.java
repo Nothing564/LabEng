7ff68826b7d074690cf65c42849520bd7ec0031d
david <david@localhost>
2014-05-18 21:49:50 -0300

    Criado as classes e boundaries de venda.

---
+++ src/livrarialabeng/LivrariaLabEng.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package livrarialabeng;
+
+import Vendas.FormVenda;
+
+/**
+ *
+ * @author David
+ */
+public class LivrariaLabEng {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FormVenda venda = new FormVenda();
+        venda.setVisible(true);
+    }
+    
+}
